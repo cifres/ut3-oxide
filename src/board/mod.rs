53de489adb783a56982abd51bb2a9f39fe136c47
Mohamed El Bakry <47481530+MohamedElBakry@users.noreply.github.com>
2025-03-15 23:00:48 +0000

    Add doctests and comments to board/mod.rs

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -14,7 +14,7 @@ pub mod iterator;
 //  (7, 2) (7, 5) (7, 8)
 // mb -> (mb // 3) + (row * 3), (mb % 3) + col * 3 
 
-const MOVE_CORRESPONDING_MINIBOARD: [u8; 81] = {
+const _MOVE_CORRESPONDING_MINIBOARD: [u8; 81] = {
     let mut a = [0u8; 81];
     let (mut y, mut x) = (0, 0);
     while y < 9 {
@@ -68,6 +68,13 @@ pub struct Board {
 
 impl Board {
 
+    /// Creates a new, empty `Board`.
+    /// 
+    /// # Example
+    /// ```
+    /// # use ut3_oxide::board::Board;
+    /// let mut board = Board::new();
+    /// ```
     pub fn new() -> Self {
         Board { 
             main_board: [0; 9],
@@ -76,6 +83,19 @@ impl Board {
         }
     }
 
+    /// Resets and zeroes the state of `Board`, as if a `new` Board was initialised.
+    ///
+    /// # Example
+    /// ```
+    /// # use ut3_oxide::board::Board;
+    /// # let mut board = Board::new();
+    /// board.do_move(4, 4, 1);
+    /// assert_eq!(board.get_cell(4, 4), 1);
+    ///
+    /// // Reset the board
+    /// board.reset();
+    /// assert_eq!(board.get_cell(4, 4), 0);
+    /// ```
     #[allow(dead_code)]
     pub fn reset(&mut self) {
         self.last_move = (flag::NEW_GAME, flag::NEW_GAME);
@@ -113,7 +133,7 @@ impl Board {
     /////* Miniboard Meta Data */////
 
     #[inline(always)]
-    pub fn get_meta_data(&self, miniboard: u8, flag_pos: u8, flag_size: u8) -> u8 {
+    fn get_meta_data(&self, miniboard: u8, flag_pos: u8, flag_size: u8) -> u8 {
         assert!(miniboard < 9);
 
         let mask = (flag_size as u32) << flag_pos;
@@ -121,7 +141,7 @@ impl Board {
     }
 
     #[inline(always)]
-    pub fn set_meta_data(&mut self, miniboard: u8, flag_pos: u8, flag_size: u8, value: u8) {
+    fn set_meta_data(&mut self, miniboard: u8, flag_pos: u8, flag_size: u8, value: u8) {
         // clear the occupying bits
         let miniboard = miniboard as usize;
         let mask = (flag_size as u32) << flag_pos;
@@ -132,18 +152,21 @@ impl Board {
         self.main_board[miniboard] |= mask;
     }
 
+    /// Returns the status of a `miniboard`.
     // NOTE: hot function
     #[inline(always)]
     pub fn get_status_of(&self, miniboard: u8) -> u8 {
         self.get_meta_data(miniboard, flag::MINIBOARD_STATUS, flag::STATUS_BIT_SIZE) 
     }
 
+    /// Sets the status of a `miniboard`.
     #[inline(always)]
     pub fn set_status_of(&mut self, miniboard: u8, value: u8) {
         assert!(value <= flag::STATUS_DRAW);
         self.set_meta_data(miniboard, flag::MINIBOARD_STATUS, flag::STATUS_BIT_SIZE, value);
     }
     
+    /// Returns the number of `miniboards` won by a `player`.
     pub fn get_miniboard_win_count_of(&self, player: u8) -> u8 {
         assert!(player == flag::X_PLAYER || player == flag::O_PLAYER);
         let offset = (player - 1) * 3;
@@ -151,9 +174,10 @@ impl Board {
         (self.xo_miniboard_win_count & mask) >> offset
     }
 
+    /// Sets the number of `miniboards` won by a `player`.
     pub fn set_miniboard_win_count_of(&mut self, player: u8, value: u8) {
         assert!(player == flag::X_PLAYER || player == flag::O_PLAYER);
-        assert!(value <= 7);    // 7 is the max for 0b111
+        assert!(value <= 7);    // 7 is the max for 0b111, a the logical max in a ut3 game
         let offset = (player - 1) * 3;
 
         // clear bits
@@ -165,7 +189,21 @@ impl Board {
         self.xo_miniboard_win_count = win_count;
     }
 
-    //NOTE: hot function
+    /// Returns the number of `moves` a player has made in a `miniboard`.
+    /// # Example
+    /// ```
+    /// # use ut3_oxide::board::Board;
+    /// let mut board = Board::new();
+    /// board.do_move(4, 4, 2);
+    /// board.do_move(4, 5, 2);
+    /// board.do_move(3, 5, 2);
+    ///
+    /// board.do_move(4, 3, 1);
+    ///
+    /// let (miniboard, player) = (4, 2);
+    /// assert_eq!(board.get_player_move_count_of(4, 2), 3);
+    /// ```
+    // NOTE: hot function
     //#[inline(always)]
     pub fn get_player_move_count_of(&self, miniboard: u8, player: u8) -> u8 {
         // maps 1 => 22 and 2 => 26
@@ -177,6 +215,7 @@ impl Board {
         self.get_meta_data(miniboard, player_flag, flag::MOVE_COUNT_BIT_SIZE)
     }
 
+    /// Sets the number of `moves` a player has made in a `miniboard`.
     //#[inline(always)]
     pub fn set_player_move_count_of(&mut self, miniboard: u8, value: u8, player: u8) {
         // maps 1 => 22 and 2 => 26
@@ -188,6 +227,7 @@ impl Board {
         self.set_meta_data(miniboard, player_flag, flag::MOVE_COUNT_BIT_SIZE, value); 
     }
 
+    /// Returns the total number of `moves` both `players` have made in a `miniboard`.
     //#[inline(always)]
     pub fn get_total_move_count_of(&self, miniboard: u8) -> u8 {
         let x_count = self.get_meta_data(miniboard, flag::MINIBOARD_MOVE_COUNT_X, flag::MOVE_COUNT_BIT_SIZE);
@@ -199,6 +239,13 @@ impl Board {
 
     /// Applies the move but doesn't check validity with `self.is_valid_move`, or apply 
     /// minboard status checks
+    /// # Example
+    /// ```
+    /// # use ut3_oxide::board::Board;
+    /// let mut board = Board::new();
+    /// board.do_move(4, 4, 2);
+    /// assert_eq!(board.get_cell(4, 4), 2);
+    /// ```
     pub fn do_move(&mut self, row: u8, column: u8, player: u8) {
         assert!(row < 9);
         assert!(column < 9);
@@ -217,7 +264,13 @@ impl Board {
 
     }
 
-    /// Returns the miniboard that move is in
+    /// Returns the `miniboard` that `move` is in.
+    /// # Example
+    /// ```
+    /// # use ut3_oxide::board::Board;
+    /// let miniboard = Board::move_miniboard(7, 6);
+    /// assert_eq!(miniboard, 8);
+    /// ```
     //#[inline]
     pub fn move_miniboard(row: u8, column: u8) -> u8 {
         debug_assert!(row < 9);
@@ -228,16 +281,33 @@ impl Board {
 
     //TODO: find better name than "corresponding"
     
-    /// Returns the miniboard number that the next move should be played in
+    /// Returns the miniboard number that the next move should be played in,
+    /// typically based on the previous move.
+    /// # Example
+    /// ```
+    /// # use ut3_oxide::board::Board;
+    /// let miniboard = Board::move_corresponding_miniboard(1, 7);
+    /// assert_eq!(miniboard, 4);
+    /// ```
     #[inline(always)]
     pub fn move_corresponding_miniboard(row: u8, column: u8) -> u8 {
-        MOVE_CORRESPONDING_MINIBOARD[((column + row * 9)) as usize]
-        //column % 3 + (row % 3 * 3)
+        //_MOVE_CORRESPONDING_MINIBOARD[((column + row * 9)) as usize]
+        column % 3 + (row % 3 * 3)
     }
 
-    // TODO: miniboard cells iterator?
-    /// Offsets a cell by `(row * 3 + column) * 2`
     /// Returns a miniboard's cells as u32
+    /// Offsets a cell by `(row * 3 + column) * 2`
+    /// # Example
+    /// ```
+    /// # use ut3_oxide::board::Board;
+    /// # let mut board = Board::new();
+    /// board.do_move(0, 0, 1);
+    /// board.do_move(0, 1, 2);
+    /// board.do_move(0, 2, 1);
+    ///
+    /// let cells = board.get_miniboard_cells(0);
+    /// assert_eq!(cells, 0b000000_000000_011001);
+    /// ```
     pub fn get_miniboard_cells(&self, miniboard: u8) -> u32  {
         assert!(miniboard < 9);
 
@@ -264,6 +334,17 @@ impl Board {
     }
 
     /// Returns the miniboard statuses and packs them into a `u32`
+    /// # Example
+    /// ```
+    /// # use ut3_oxide::board::Board;
+    /// # let mut board = Board::new();
+    /// board.set_status_of(1, 1);
+    /// board.set_status_of(4, 1);
+    /// board.set_status_of(8, 2);
+    ///
+    /// let statuses = board.get_miniboard_statuses();
+    /// assert_eq!(statuses, 0b100000_000100_000100);
+    /// ```
     pub fn get_miniboard_statuses(&self) -> u32 {
         let mut statuses = 0u32;
         for mb in 0..self.main_board.len() as u8 {
@@ -276,7 +357,7 @@ impl Board {
         statuses
     }
 
-    pub fn get_miniboard_statuses_iter(&self) -> iterator::MiniboardStatusesIterator {
+    pub fn _get_miniboard_statuses_iter(&self) -> iterator::MiniboardStatusesIterator {
         let bitfield = self.get_miniboard_statuses(); 
 
         iterator::MiniboardStatusesIterator::new(bitfield)
